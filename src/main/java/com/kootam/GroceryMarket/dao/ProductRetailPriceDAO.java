51484e51f40d7e06d33d6588a85dcedc7d4ded35
Richard Thomas <richard.t.alappat@gmail.com>
2022-12-29 19:31:56 +0530

    created productRetailPriceDAO

---
+++ src/main/java/com/kootam/GroceryMarket/dao/ProductRetailPriceDAO.java
@@ -0,0 +1,9 @@
+package com.kootam.GroceryMarket.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kootam.GroceryMarket.model.ProductRetailPrice;
+
+public interface ProductRetailPriceDAO extends JpaRepository<ProductRetailPrice,Long>{
+
+}
