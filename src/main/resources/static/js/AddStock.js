3a881c72ddb85a850aec9e3663469b166d9bd530
Richard Thomas <richard.t.alappat@gmail.com>
2023-01-03 17:41:05 +0530

    added method to fetch unit of Quantity

--- src/main/resources/static/js/AddStock.js
+++ src/main/resources/static/js/AddStock.js
@@ -2,51 +2,72 @@ window.onload = (loadEvent) =>{
     fetch ("/itemCategory/")
     .then(response => response.json())
     .then(jsonData => {
-            //convet to json object
-            var jsonObject = JSON.stringify(jsonData);
-            console.log(jsonObject);
-            //convert to json array
-            var facultyObject =JSON.parse(jsonObject);
-            console.log(facultyObject);
-            // const map = new Map(Object.entries(facultyObject));
-            // console.log(map);
+        //convet to json object
+        var jsonObject = JSON.stringify(jsonData);
+        console.log(jsonObject);
+        //convert to json array
+        var facultyObject =JSON.parse(jsonObject);
+        console.log(facultyObject);
+        // const map = new Map(Object.entries(facultyObject));
+        // console.log(map);
             
-            let jsonlength = facultyObject.length;
-            console.log(jsonlength);
-            for(var index in facultyObject){
-                console.log(index);
-                console.log(facultyObject[index].itemCategoryId);
-                console.log(facultyObject[index].itemCategoryName)
-                let itemCatId = facultyObject[index].itemCategoryId;
-                let itemCatName = facultyObject[index].itemCategoryName;
-                document.getElementById("itemCategory").options.add(new Option(itemCatName,itemCatId));
-            }
+        let jsonlength = facultyObject.length;
+        console.log(jsonlength);
+        for(var index in facultyObject){
+            console.log(index);
+            console.log(facultyObject[index].itemCategoryId);
+            console.log(facultyObject[index].itemCategoryName)
+            let itemCatId = facultyObject[index].itemCategoryId;
+            let itemCatName = facultyObject[index].itemCategoryName;
+            document.getElementById("itemCategory").options.add(new Option(itemCatName,itemCatId));
+        }
 
             
-        });
-        fetch ("//Vendor/listVendors")
+    });
+    fetch ("//Vendor/listVendors")
     .then(response => response.json())
     .then(jsonData => {
-            //convet to json object
-            var jsonObject = JSON.stringify(jsonData);
-            console.log(jsonObject);
-            //convert to json array
-            var facultyObject =JSON.parse(jsonObject);
-            console.log(facultyObject);
-            // const map = new Map(Object.entries(facultyObject));
-            // console.log(map);
+        //convet to json object
+        var jsonObject = JSON.stringify(jsonData);
+        console.log(jsonObject);
+        //convert to json array
+        var facultyObject =JSON.parse(jsonObject);
+        console.log(facultyObject);
+        // const map = new Map(Object.entries(facultyObject));
+        // console.log(map);
             
-            let jsonlength = facultyObject.length;
-            console.log(jsonlength);
-            for(var index in facultyObject){
-                console.log(index);
-                console.log(facultyObject[index].vendorId);
-                console.log(facultyObject[index].vendorName)
-                let venId = facultyObject[index].vendorId;
-                let venName = facultyObject[index].vendorName;
-                document.getElementById("vendor").options.add(new Option(venName,venId));
-            }
+        let jsonlength = facultyObject.length;
+        console.log(jsonlength);
+        for(var index in facultyObject){
+            console.log(index);
+            console.log(facultyObject[index].vendorId);
+            console.log(facultyObject[index].vendorName)
+            let venId = facultyObject[index].vendorId;
+            let venName = facultyObject[index].vendorName;
+            document.getElementById("vendor").options.add(new Option(venName,venId));
+        }
 
             
-        });
+    });
+    fetch ("/StockItem/UnitOfQuantity")
+    .then(response => response.json())
+    .then(jsonData => {
+        //convet to json object
+        var jsonObject = JSON.stringify(jsonData);
+        console.log(jsonObject);
+        //convert to json array
+        var facultyObject =JSON.parse(jsonObject);
+        console.log(facultyObject);
+        const map = new Map(Object.entries(facultyObject));
+        console.log(map);
+            
+
+        const keyIterator = map.keys();
+        const valueIterator = map.values();
+        for( var i = 0;i<map.size; i++){
+            var value = valueIterator.next().value;
+            var key = keyIterator.next().value;
+            document.getElementById("unitOfQuantity").options.add(new Option(key,value));
+        }
+    });
 }
