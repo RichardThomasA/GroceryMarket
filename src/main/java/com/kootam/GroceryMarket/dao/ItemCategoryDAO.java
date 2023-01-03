08a225450d59afe032ea3141e0107421ce2faaa8
Richard Thomas <richard.t.alappat@gmail.com>
2023-01-03 16:32:41 +0530

    renamed dao of StockCategory to ItemCategory

--- src/main/java/com/kootam/GroceryMarket/dao/ItemCategoryDAO.java
+++ src/main/java/com/kootam/GroceryMarket/dao/ItemCategoryDAO.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kootam.GroceryMarket.model.ItemCategory;
 
-public interface StockCategoryDAO extends JpaRepository<ItemCategory,Long>{
+public interface ItemCategoryDAO extends JpaRepository<ItemCategory,Long>{
 
 }
