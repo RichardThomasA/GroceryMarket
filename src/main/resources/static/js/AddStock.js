38d06a7cbd50b113d4cf93886f3be2d48b527b4c
Richard Thomas <richard.t.alappat@gmail.com>
2023-01-03 16:34:39 +0530

    added addStock.js file

---
+++ src/main/resources/static/js/AddStock.js
@@ -0,0 +1,52 @@
+window.onload = (loadEvent) =>{
+    fetch ("/itemCategory/")
+    .then(response => response.json())
+    .then(jsonData => {
+            //convet to json object
+            var jsonObject = JSON.stringify(jsonData);
+            console.log(jsonObject);
+            //convert to json array
+            var facultyObject =JSON.parse(jsonObject);
+            console.log(facultyObject);
+            // const map = new Map(Object.entries(facultyObject));
+            // console.log(map);
+            
+            let jsonlength = facultyObject.length;
+            console.log(jsonlength);
+            for(var index in facultyObject){
+                console.log(index);
+                console.log(facultyObject[index].itemCategoryId);
+                console.log(facultyObject[index].itemCategoryName)
+                let itemCatId = facultyObject[index].itemCategoryId;
+                let itemCatName = facultyObject[index].itemCategoryName;
+                document.getElementById("itemCategory").options.add(new Option(itemCatName,itemCatId));
+            }
+
+            
+        });
+        fetch ("//Vendor/listVendors")
+    .then(response => response.json())
+    .then(jsonData => {
+            //convet to json object
+            var jsonObject = JSON.stringify(jsonData);
+            console.log(jsonObject);
+            //convert to json array
+            var facultyObject =JSON.parse(jsonObject);
+            console.log(facultyObject);
+            // const map = new Map(Object.entries(facultyObject));
+            // console.log(map);
+            
+            let jsonlength = facultyObject.length;
+            console.log(jsonlength);
+            for(var index in facultyObject){
+                console.log(index);
+                console.log(facultyObject[index].vendorId);
+                console.log(facultyObject[index].vendorName)
+                let venId = facultyObject[index].vendorId;
+                let venName = facultyObject[index].vendorName;
+                document.getElementById("vendor").options.add(new Option(venName,venId));
+            }
+
+            
+        });
+}
