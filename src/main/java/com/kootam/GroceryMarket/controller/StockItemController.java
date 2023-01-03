0152469823c0f998d437854545860458ea3b334b
Richard Thomas <richard.t.alappat@gmail.com>
2023-01-03 17:40:36 +0530

    created method to return unitOfQuantity as JSON

--- src/main/java/com/kootam/GroceryMarket/controller/StockItemController.java
+++ src/main/java/com/kootam/GroceryMarket/controller/StockItemController.java
@@ -1,8 +1,14 @@
 package com.kootam.GroceryMarket.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kootam.GroceryMarket.model.StockItem;
 
 @Controller
 @RequestMapping("/StockItem")
@@ -11,5 +17,14 @@ public class StockItemController {
 	@GetMapping("/addStockItem")
 	public String addStockItem() {
 		return "/Stock/AddStock";
+	}
+	
+	@GetMapping(value = "/UnitOfQuantity")
+	public ResponseEntity<Map<String,String>> getUnitOfQuantity() {
+		Map<String,String> unitOfQuantities = new HashMap<String,String>();
+		unitOfQuantities.put(StockItem.ITEM_GRAM,StockItem.ITEM_GRAM);
+		unitOfQuantities.put(StockItem.ITEM_KG,StockItem.ITEM_KG);
+		unitOfQuantities.put(StockItem.ITEM_NOS,StockItem.ITEM_NOS);
+		return ResponseEntity.ok(unitOfQuantities);
 	}
 }
