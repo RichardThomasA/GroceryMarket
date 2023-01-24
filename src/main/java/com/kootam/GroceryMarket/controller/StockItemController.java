8657218c696a04664cdcfa543a273d61f1ab450d
Richard Thomas <richard.t.alappat@gmail.com>
2023-01-24 11:44:40 +0530

    added the viewAllStock method

--- src/main/java/com/kootam/GroceryMarket/controller/StockItemController.java
+++ src/main/java/com/kootam/GroceryMarket/controller/StockItemController.java
@@ -3,11 +3,13 @@ package com.kootam.GroceryMarket.controller;
 import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -119,5 +121,12 @@ public class StockItemController {
 		stockItem.setUnitOfQuantity(unitOfQuantity);
 		
 		stockItemDAO.save(stockItem);
+	}
+	
+	@GetMapping(value = "/viewAllStock")
+	public String viewAllStock(Model model) {
+		List<StockItem> stockItemList = stockItemDAO.findAll();
+		model.addAttribute("stockItemList",stockItemList);
+		return "/Stock/ViewAllStock";
 	}
 }
